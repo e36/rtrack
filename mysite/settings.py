3ccddad2bd16d48e491faed998421ee6940fab66
e36 <Tqbfjotld1>
2016-12-11 16:58:08 -0600

    Slack integration Laid the basic foundation for integrating this site with slack

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'e6n#m+_aqsa+)2g_4f=l4hm@s6jmaeekxgg@6*yerocdm=uvg@'
+SLACK_TOKEN = 'token'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
